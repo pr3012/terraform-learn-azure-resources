6c566b6369457386c15381cc98c6ba7853987b44
Prasanth Marri (C) <PMarri@pscu.com>
2026-06-07 11:26:07 +0530

    fixed the issue

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,3 +1,10 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
   backend "azurerm" {}
 }
--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,4 +1,4 @@
-resource "azure_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "vnet" {
     name = "vnet-${var.environment}"
     location = var.location
     resource_group_name = var.resource_group_name
