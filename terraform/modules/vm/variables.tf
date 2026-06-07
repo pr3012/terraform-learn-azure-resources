27c49ed3f5570ee533ecd48fbe75a732c74ec0aa
Prasanth Marri (C) <PMarri@pscu.com>
2026-06-07 11:36:36 +0530

    fixed admin password issue

--- terraform/modules/vm/variables.tf
+++ terraform/modules/vm/variables.tf
@@ -2,6 +2,6 @@ variable "resource_group_name" {}
 variable "location" {}
 variable "vm_name" {}
 variable "admin_username" {}
-variable "admin_password" {}
+variable "admin_password" {Supriya@2409}
 variable "subnet_id" {}
 variable "environment" {}
