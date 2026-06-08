20e1df99da9ea1f34f916958da727cbf4a102eb3
Prasanth Marri (C) <PMarri@pscu.com>
2026-06-08 09:28:45 +0530

    fixed subnet issue

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-${var.prefix}-${var.environment}"
   location            = var.location
   resource_group_name = var.resource_group_name
-  address_space       = ["10.0.0.0/24"]
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
